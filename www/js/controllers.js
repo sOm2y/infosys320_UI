df1693ec16cbc1a89813b16e4f1dffd904b2f810
sOm2y <sam@som2y.com>
2014-09-14 18:03:22 +1200

    mobile_UI_for infosys320
    using ionic and angular.js

---
+++ www/js/controllers.js
@@ -0,0 +1,21 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
+
+function MyCtrl($scope, $ionicSlideBoxDelegate) {
+  $scope.nextSlide = function() {
+    $ionicSlideBoxDelegate.next();
+  }
+}
