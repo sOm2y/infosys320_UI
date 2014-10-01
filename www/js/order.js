54264b550c7e443dbfcdb519ef29197c151318bc
sOm2y <samyin1990@gmail.com>
2014-10-01 22:46:35 +1300

    swipe function need to be fixed

--- www/js/order.js
+++ www/js/order.js
@@ -1,9 +1,73 @@
-angular.module('ionicApp', ['ionic'])
+var app=angular.module('order', ['ionic']);
 
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+app.controller('OrderCtrl', function($scope, $ionicSideMenuDelegate) {
  
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
+  
+});
+//
+//app.controller('ListController',function(){
+//    
+//
+//
+//});
+//
+//app.controller('TabController',function(){
+//    this.tabs=item;
+//
+//
+//
+//});
+//
+//var item={
+//    name:"1"
+//
+//}
+
+app.controller('BeverageCtrl', function($scope) {
+  // don't be scared by the image value, its just datauri
+  
+  $scope.items = [
+    { id: 1, album: 'Gotta Be Somebody', artist: 'Nickelback', image:'/img/beverage1.jpg'},
+    { id: 2, album: 'Dark Horse', artist: 'Nickelback', image:'/img/beverage2.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 4, album: 'All The Right Reasons', artist: 'Nickelback', image:'/img/beverage3.jpg' }
+  ];
+  
+});
+app.controller('FoodCtrl', function($scope) {
+  // don't be scared by the image value, its just datauri
+  
+  $scope.items = [
+    { id: 1, album: 'Gotta Be Somebody', artist: 'Nickelback', image:'/img/beverage1.jpg'},
+    { id: 2, album: 'Dark Horse', artist: 'Nickelback', image:'/img/beverage2.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 4, album: 'All The Right Reasons', artist: 'Nickelback', image:'/img/beverage3.jpg' }
+  ];
+  
+});
+app.controller('CartCtrl', function($scope) {
+  // don't be scared by the image value, its just datauri
+  
+  $scope.items = [
+    { id: 1, album: 'Gotta Be Somebody', artist: 'Nickelback', image:'/img/beverage1.jpg'},
+    { id: 2, album: 'Dark Horse', artist: 'Nickelback', image:'/img/beverage2.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 3, album: 'Someday', artist: 'Nickelback', image:'/img/beverage3.jpg' },
+    { id: 4, album: 'All The Right Reasons', artist: 'Nickelback', image:'/img/beverage3.jpg' }
+  ];
   
 });
